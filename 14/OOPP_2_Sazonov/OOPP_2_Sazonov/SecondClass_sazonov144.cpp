afc182b72bc665285cb094566392f15de364c36e
sazonovvladimir <owl29896@rambler.ru>
2016-10-09 15:40:49 +0300

    ()

---
+++ 14/OOPP_2_Sazonov/OOPP_2_Sazonov/SecondClass_sazonov144.cpp
@@ -0,0 +1,22 @@
+#include "stdafx.h"
+#include "SecondClass_sazonov144.h"
+
+
+SecondClass_sazonov144::SecondClass_sazonov144()
+{
+}
+
+
+SecondClass_sazonov144::~SecondClass_sazonov144()
+{
+}
+
+void SecondClass_sazonov144::SimpFun()
+{
+	cout << "Simple function of DERIVED CLASS" << endl;
+}
+
+void SecondClass_sazonov144::VirFun()
+{
+	cout << "Virtual function of DERIVED CLASS" << endl;
+}
---
+++ 14/OOPP_2_Sazonov/OOPP_2_Sazonov/SecondClass_sazonov144.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "FirstClass_sazonov144.h"
+class SecondClass_sazonov144 :
+	public FirstClass_sazonov144
+{
+public:
+	SecondClass_sazonov144();
+	~SecondClass_sazonov144();
+
+	void SimpFun();
+	void virtual VirFun();
+};
+
